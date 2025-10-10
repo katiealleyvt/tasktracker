0840d7cee707eb256f5566bb38207ee9210c65ac
Katie Alley <112027177+katiealleyvt@users.noreply.github.com>
2025-10-10 19:13:04 -0400

    Add cors origin var

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   })
 );
 app.listen(3000, () => {
